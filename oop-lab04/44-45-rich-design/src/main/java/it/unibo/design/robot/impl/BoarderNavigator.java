d1603af3cdd69591696d75c72863ad782f2b2696
Karim-EK <karim.elkholy@studio.unibo.it>
2024-10-14 15:49:19 +0200

    waiting for correction es 45

---
+++ oop-lab04/44-45-rich-design/src/main/java/it/unibo/design/robot/impl/BoarderNavigator.java
@@ -0,0 +1,35 @@
+package it.unibo.design.robot.impl;
+
+public class BoarderNavigator extends AbstractComponent {
+
+    @Override
+    public void activate() {
+        if (isOperational()) {
+
+            System.out.println("Ready to explore the boarders of the environment");
+
+            System.out.println("Now moving to the up boarder");
+            do {
+                this.getRobot().moveUp();
+            } while (!this.getRobot().moveUp());
+
+            System.out.println("Now moving to the right boarder");
+            do {
+                this.getRobot().moveRight();
+            } while (!this.getRobot().moveRight());
+
+            System.out.println("Now moving to the down boarder");
+            do {
+                this.getRobot().moveDown();
+            } while (!this.getRobot().moveDown());
+
+            System.out.println("Now moving to the left boarder");
+            do {
+                this.getRobot().moveLeft();
+            } while (!this.getRobot().moveLeft());
+
+            System.out.println("Navigated trough all the boarders of the environment");
+        }
+    }
+    
+}
