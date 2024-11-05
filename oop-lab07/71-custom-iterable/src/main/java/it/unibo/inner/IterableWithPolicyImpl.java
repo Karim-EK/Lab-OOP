288d1fc4d17415a6ee0268b89866907632814396
Karim-EK <karim.elkholy@studio.unibo.it>
2024-11-05 12:53:02 +0100

    lab 7 in corso

---
+++ oop-lab07/71-custom-iterable/src/main/java/it/unibo/inner/IterableWithPolicyImpl.java
@@ -0,0 +1,66 @@
+package it.unibo.inner;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    private T[] array;
+    private Predicate<T> predicate;
+
+    public IterableWithPolicyImpl(T[] array) {
+        this.array = array;
+        this.predicate = new Predicate<T>() {
+
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }    
+        };
+    }
+
+    public IterableWithPolicyImpl(T[] array, Predicate<T> predicate) {
+        this(array);
+        this.predicate = predicate;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new InnerIterator();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        this.predicate = filter;
+    }
+
+    private class InnerIterator implements Iterator<T> {
+
+        private int current;
+
+        public InnerIterator() {
+        this.current = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (current < IterableWithPolicyImpl.this.array.length) {
+                if (IterableWithPolicyImpl.this.predicate.test(IterableWithPolicyImpl.this.array[current])) {
+                    return true;
+                } 
+                current ++;
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) {
+                return IterableWithPolicyImpl.this.array[current++];
+            }
+            throw new NoSuchElementException();
+        }
+   }
+}
